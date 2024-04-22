372212ad3a98a69bc0e4fe019a8e87e0790e54e9
Linda Nab <lindanab4@gmail.com>
2024-04-22 13:23:51 +0200

    reorganise to using input and output folders

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -2,16 +2,20 @@
 # input: ./config-dummy-data.toml
 # output: ./dummy-data.csv
 
+import os
 from metasyn import MetaFrame
 
+# Create subdir "output" where output is saved
+os.mkdir("output")
+
 # Create MetaFrame from config file 
-config = MetaFrame.from_config("config-dummy-data.toml")
+config = MetaFrame.from_config("input/config-dummy-data.toml")
 
 # Export config file to json for reference
-config.to_json("gmf-dummy-data.json")
+config.to_json("output/gmf-dummy-data.json")
 
 # Generate a new DataFrame from the MetaFrame
 df_synth = config.synthesize()
 
 # Export DataFrame to csv
-df_synth.write_csv("dummy-data.csv")
+df_synth.write_csv("output/dummy-data.csv")
